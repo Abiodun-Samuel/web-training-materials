9d50df80ebdb8ea808da7a4fdd46c5ce893627c1
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-07-29 16:28:33 +0100

    async code

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,18 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import axios from "axios";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+let btn = document.getElementById("counter");
+let display = document.getElementById("display");
+
+btn.addEventListener("click", async () => {
+  let { data } = await axios.get("https://fakestoreapi.com/products");
+
+  for (let item of data) {
+    display.innerHTML += `
+    <div>
+      <h4>Title: ${item.title}</h4>
+      <img src=${item.image} height='200px' width='200px'/>
+      <p>Cat: ${item.category}</p>
+      <p>Des: ${item.description}</p>
+      <p>Price: ${item.price}</p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+  `;
+  }
+});
