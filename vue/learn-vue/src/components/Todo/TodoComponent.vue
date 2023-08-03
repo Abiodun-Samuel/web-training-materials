75fecd953ffdbb8429e2bcbc0bca201012e777b1
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-08-03 18:04:48 +0100

    updated todo list functionality

--- vue/learn-vue/src/components/Todo/TodoComponent.vue
+++ vue/learn-vue/src/components/Todo/TodoComponent.vue
@@ -6,6 +6,7 @@
   <div id="createTodo">
     <input @keypress.enter="submitTodo" type="text" placeholder="todo item" v-model="todoItem" />
     <button v-on:click="submitTodo">Submit</button>
+    <button @click="clearTodo">Clear</button>
   </div>
 
   <div style="margin: 10px 0px" v-for="(todo, index) in todos" :key="todo + index">
@@ -26,10 +27,25 @@ const deleteTodo = (i) => {
   })
 }
 
+const clearTodo = () => {
+  todos.value = []
+}
+
 const submitTodo = () => {
-  if (todoItem.value?.trim().length > 0) {
+  let validate = todoItem.value?.trim().length > 0
+
+  // let check = todos.value.includes(todoItem.value)
+  let check = todos.value.filter((item) => {
+    return item.toLowerCase() == todoItem.value.toLowerCase()
+  })
+
+  if (validate && check.length == 0) {
+    //(validate === true && check === false
     todos.value.push(todoItem.value)
     todoItem.value = null
+  }
+  if (check.length > 0) {
+    alert(`${todoItem.value} already exist in your todo list`)
   }
 }
 </script>
